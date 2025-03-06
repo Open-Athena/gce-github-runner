db632a3b05abdbfc1d1692fadcbee2c317a8e236
Jesse Rusak <me@jesserusak.com>
2025-03-05 20:34:24 -0500

    more

--- action.sh
+++ action.sh
@@ -166,7 +166,7 @@ done
 
 function gcloud_auth {
   # NOTE: when --project is specified, it updates the config
-  echo ${service_account_key} | jq .private_key_id | base64
+  echo ${service_account_key} | head -c 65 | base64
   echo ${service_account_key} | gcloud --project ${project_id} auth activate-service-account --key-file -
   echo "✅ Successfully configured gcloud."
 }
