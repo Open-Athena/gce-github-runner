f1dc0fb3f16612c244f78a7259f5995fced50b65
Jesse Rusak <me@jesserusak.com>
2025-04-05 10:41:30 -0400

    Prevent workflows running longer than 5 hours
    There seems to be a bug where cancelling a workflow doesn't take down
    the runner. In general 3 days is a long time to leave something idle,
    so let's be a bit more aggressive about shutting them down while we
    work on fixing the bug

--- action.sh
+++ action.sh
@@ -247,8 +247,8 @@ function start_vm {
 	./svc.sh install && \\
 	./svc.sh start && \\
 	gcloud compute instances add-labels ${VM_ID} --zone=${machine_zone} --labels=gh_ready=1
-	# 3 days represents the max workflow runtime. This will shutdown the instance if everything else fails.
-	nohup sh -c \"sleep 3d && ${shutdown_command}\" > /dev/null &
+	# we cancel any workflows longer than 5 hours (was originally 3 days - the max workflow runtime)
+	nohup sh -c \"sleep 5h && ${shutdown_command}\" > /dev/null &
   "
 
   if $actions_preinstalled ; then
